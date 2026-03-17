15382c4d2987e3920b1292c8836d52f7e1670402
ACT-STUDENT <ACT-STUDENT@LAB1-PC3>
2026-03-17 13:40:48 +0800

    MOVIE

---
+++ CinemaHub/Models/Category.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace CinemaHub.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryID { get; set; }
+
+    }
+}
--- CinemaHub/Models/Movie.cs
+++ CinemaHub/Models/Movie.cs
@@ -1,7 +1,18 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace CinemaHub.Models
 {
     public class Movie
     {
+        [Key]
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public 
+        public string? Description { get; set; }
+        public string? Director { get; set; }
+        public DateTime ReleaseDate { get; set; }
     }
 }
